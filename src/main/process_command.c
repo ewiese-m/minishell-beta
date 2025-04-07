a3cdaf0675143b5155786df0ed217c012cf3fe56
ewiese-m <ewiese-m@student.42luxembourg.lu>
2025-04-07 15:19:37 +0200

    error que casi me da un infarto + actualizacion .gitignore

--- src/main/process_command.c
+++ src/main/process_command.c
@@ -6,7 +6,7 @@
 /*   By: ewiese-m <ewiese-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 18:16:01 by ewiese-m          #+#    #+#             */
-/*   Updated: 2025/04/06 13:42:54 by ewiese-m         ###   ########.fr       */
+/*   Updated: 2025/04/07 15:18:02 by ewiese-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,19 +61,40 @@ static int	process_valid_command(t_command *cmds, t_env *env_list,
 int	process_command(char *line, t_env *env_list, char **env_copy)
 {
 	t_command	*cmds;
-	//int			exit_status;
+	int			exit_status;
 
-	//exit_status = 0;
+	// exit_status = 0;
 	if (!line || *line == '\0')
 		return (0);
 	add_history(line);
 	cmds = ft_parse_input(line, env_list);
 	if (cmds)
 	{
-		display_command_info(cmds);
-		//exit_status = process_valid_command(cmds, env_list, env_copy);
+		// display_command_info(cmds);
+		exit_status = process_valid_command(cmds, env_list, env_copy);
 		ft_free_cmdlist(&cmds);
 	}
 	free(line);
-	return (0);
+	return (exit_status);
 }
+/*  For debug the paxer/lexer part:
+int	process_command(char *line, t_env *env_list, char **env_copy)
+{
+	t_command	*cmds;
+	int			exit_status;
+
+	// exit_status = 0;
+	if (!line || *line == '\0')
+		return (0);
+	add_history(line);
+	cmds = ft_parse_input(line, env_list);
+	if (cmds)
+	{
+		// display_command_info(cmds);
+		exit_status = process_valid_command(cmds, env_list, env_copy);
+		ft_free_cmdlist(&cmds);
+	}
+	free(line);
+	return (exit_status);
+}
+ */

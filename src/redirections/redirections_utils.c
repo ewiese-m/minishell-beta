db4220a27147a2874ac4632960afa8b9925c8439
ewiese-m <ewiese-m@student.42luxembourg.lu>
2025-04-16 11:28:23 +0200

    correccion.

--- src/redirections/redirections_utils.c
+++ src/redirections/redirections_utils.c
@@ -6,7 +6,7 @@
 /*   By: ewiese-m <ewiese-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 10:03:29 by ewiese-m          #+#    #+#             */
-/*   Updated: 2025/04/15 13:15:16 by ewiese-m         ###   ########.fr       */
+/*   Updated: 2025/04/16 10:40:03 by ewiese-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,9 @@ void	redirections_error_1(char *file)
 	ft_putstr_fd("minishell: ", 2);
 	ft_putstr_fd(file, 2);
 	ft_putstr_fd(": ", 2);
-	ft_putstr_fd(strerror(errno), 2);
+	if (errno == EACCES)
+		ft_putstr_fd("Permission denied", 2);
+	else
+		ft_putstr_fd(strerror(errno), 2);
 	ft_putstr_fd("\n", 2);
 }

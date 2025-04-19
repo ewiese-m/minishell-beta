4361d555b382dd32e94f6e12514d276abd27d496
ewiese-m <ewiese-m@student.42luxembourg.lu>
2025-04-19 10:15:47 +0200

    ayuda de massimo

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 # Compiler and flags
 CC = cc
-CFLAGS = -Wall -Wextra -Werror
+CFLAGS = -Wall -Wextra -Werror -g
 #CFLAGS = -Wall -Wextra -g
 #CFLAGS = -Wall -Wextra -Werror -g -fsanitize=address
 INCLUDE = -I./includes -I./libft
--- src/pipelines/pipeline_utils.c
+++ src/pipelines/pipeline_utils.c
@@ -6,7 +6,7 @@
 /*   By: ewiese-m <ewiese-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 15:47:13 by ewiese-m          #+#    #+#             */
-/*   Updated: 2025/04/16 14:40:39 by ewiese-m         ###   ########.fr       */
+/*   Updated: 2025/04/17 17:23:31 by ewiese-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,16 @@ void	close_all_pipes(t_pipeline *pipeline)
 /**
  * waits for all child processes to complete
  */
+
+static int	parse_raw_exit_status_massimo(int status)
+{
+	if (WIFEXITED(status))
+		return (WEXITSTATUS(status));
+	else if (WIFSIGNALED(status))
+		return (128 + WTERMSIG(status));
+	return (1);
+}
+
 int	wait_for_commands(t_pipeline *pipeline, pid_t *pids)
 {
 	int	i;
@@ -57,7 +67,7 @@ int	wait_for_commands(t_pipeline *pipeline, pid_t *pids)
 		if (waitpid(pids[i], &status, 0) == -1)
 			handle_wait_error();
 		else if (i == pipeline->cmd_count - 1)
-			last_status = WEXITSTATUS(status);
+			last_status = parse_raw_exit_status_massimo(status);
 		i++;
 	}
 	return (last_status);
